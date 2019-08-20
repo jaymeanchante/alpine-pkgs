3416fcb622dae211cb4d3c7f90cc38a2c1449354
Jayme Anchante <jayme.anchante@disroot.org>
2019-08-20 17:16:01 -0300

    :tada: initial commit

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # alpine-pkgs
+
+Alpine package information as a CSV for the `armv7` architecture
+
+Currently, you can only query [alpine packages](https://pkgs.alpinelinux.org/packages) using the package name. However, you can't search using the package description (it appears as you hover your mouse over the package name or if you click on the package). As a bought a [Nokia N900](https://wiki.maemo.org/Nokia_N900) to install and use [PostmarketOS](https://postmarketos.org/), I had the need to discover and install new packages for the `armv7` architecture in the alpine repositories.
+
+This repository contains an updated database in the relative path - `./data.csv` - or in the [URL](https://raw.githubusercontent.com/jayme-anchante/alpine-pkgs/master/data.csv), if you want to update the data yourself you can follow the instructions below:
+
+# Installation
+
+1. Clone this repository:
+
+```
+git clone https://github.com/jayme-anchante/alpine-pkgs.git
+```
+
+2. Install dependencies and pull the data:
+
+```
+cd alpine-pkgs && \
+python3 -m venv venv && \
+source venv/bin/activate && \
+pip3 install -U pip && \
+pip3 install -r requirements.txt && \
+python3 get_data.py
+```
+
+3. (Optional) Run analysis:
+
+```
+jupyter-notebook
+```
+
+and check `localhost:8888`
---
+++ get_data.py
@@ -0,0 +1,74 @@
+import os
+import time
+from glob import glob
+from urllib.request import urlopen
+from urllib.request import Request
+import pandas as pd
+from tqdm import tqdm
+from bs4 import BeautifulSoup
+
+def make_headers():
+    headers = {}
+    headers["User-Agent"] = "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:48.0) Gecko/20100101 Firefox/48.0"
+    return headers
+
+def make_request(page):
+    req = Request(url.format(page=page), headers=make_headers())
+    html = urlopen(req).read().decode("utf-8")
+    return html
+
+def make_soup(html):
+    soup = BeautifulSoup(html, "html.parser")
+    table = soup.find(attrs={"class": "table-responsive"})
+    a = table.findAll(name="a")
+    elements = {}
+    for elem in ["license", "branch", "repo", "arch", "maintainer", "bdate"]:
+        elements[elem] = table.findAll(attrs={"class": elem})
+    return a, elements
+
+def parse_a(a):
+    ind = 0
+    for i, j, k in zip(range(0, len(a)-1, 3), range(1, len(a), 3), range(2, len(a)+1, 3)):
+        df.loc[ind, "description"] = a[i]["aria-label"]
+        df.loc[ind, "pkg_url"] = a[i]["href"]
+        df.loc[ind, "name"] = a[i].string
+        df.loc[ind, "version"] = a[j].string
+        df.loc[ind, "alpine_url"] = a[k]["href"]
+        ind += 1
+    pass
+
+def parse_elements(elements):
+    for elem in elements:
+        for i in range(len(elements[elem])):
+            df.loc[i, elem] = elements[elem][i].string
+    pass
+
+def unite_files():
+    df = pd.DataFrame()
+    files = glob("csvs/*.csv")
+    for file in tqdm(files, desc="file"):
+        temp = pd.read_csv(file, sep=";")
+        df = pd.concat([df, temp], ignore_index=True)
+    df.to_csv("data.csv", sep=";", index=False)
+    pass
+
+def clean_files():
+    files = glob("csvs/*.csv")
+    for file in tqdm(files, desc="deleting"):
+        os.remove(file)
+    os.rmdir("csvs/")
+    pass
+
+if __name__ == "__main__":
+    os.makedirs("csvs", exist_ok=True)
+    url = "https://pkgs.alpinelinux.org/packages?page={page}&branch=edge&arch=armv7"
+    for i in tqdm(range(1, 288), desc="scrapping"):
+        df = pd.DataFrame()
+        html = make_request(page=i)
+        a, elements = make_soup(html)
+        parse_a(a)
+        parse_elements(elements)
+        df.to_csv("csvs/"+str(i)+".csv", sep=";", index=False)
+        time.sleep(1)
+    unite_files()
+    clean_files()
